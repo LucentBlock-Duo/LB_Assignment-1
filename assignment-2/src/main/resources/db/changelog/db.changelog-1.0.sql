38fa0b04fbd4eb7c6e2ecc6b71fe8dc9f38d82c9
0-tae <100774819+0-tae@users.noreply.github.com>
2023-07-03 11:34:02 +0900

    fix : liquibase ver 1.1 삭제, 1.0 수정

--- assignment-2/src/main/resources/db/changelog/db.changelog-1.0.sql
+++ assignment-2/src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -161,3 +161,12 @@ INSERT INTO repair_man (id, name, license_id) VALUES
                                                   (2, 'Repairman B', 2);
 
 SELECT * FROM user;
+
+-- changeset 0tae:changelog-reserve.1.1
+ALTER TABLE reserve MODIFY id INT NOT NULL AUTO_INCREMENT;
+
+-- changeset 0tae:changelog-login_challenge.1.1
+ALTER TABLE login_challenge MODIFY id INT NOT NULL AUTO_INCREMENT;
+
+-- changeset 0tae:changelog-signup_code_challenge.1.1
+ALTER TABLE signup_code_challenge MODIFY id INT NOT NULL AUTO_INCREMENT;
