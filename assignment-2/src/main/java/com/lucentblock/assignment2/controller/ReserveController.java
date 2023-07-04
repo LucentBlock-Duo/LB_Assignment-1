a0795b5f601c48a4836686a2b09c04028f718976
0-tae <100774819+0-tae@users.noreply.github.com>
2023-07-04 16:50:37 +0900

    refactor : ReserveController 디렉토리 이동 및 수정

--- assignment-2/src/main/java/com/lucentblock/assignment2/controller/ReserveController.java
+++ assignment-2/src/main/java/com/lucentblock/assignment2/controller/ReserveController.java
@@ -1,8 +1,9 @@
-package com.lucentblock.assignment2.service;
+package com.lucentblock.assignment2.controller;
 
 
 import com.lucentblock.assignment2.entity.Reserve;
 import com.lucentblock.assignment2.model.*;
+import com.lucentblock.assignment2.service.ReserveService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -19,22 +20,22 @@ public class ReserveController {
         this.reserveService = reserveService;
     }
 
-    @PostMapping("/api/reserve/create")
+    @PostMapping("/api/reserve")
     private ResponseReserveDTO create(@Valid @RequestBody CreateRequestReserveDTO dto){
         return reserveService.createReserve(dto);
     }
 
-    @GetMapping("/api/reserve/read") // 검증완료
+    @GetMapping("/api/reserve") // 검증완료
     private List<ResponseReserveDTO> read(@RequestParam Long carId){
         return reserveService.findReserveByCarId(carId).stream().map(Reserve::toDto).toList();
     }
 
-    @PostMapping("/api/reserve/update") // 검증완료
+    @PutMapping("/api/reserve") // 검증완료
     private ResponseReserveDTO update(@RequestBody UpdateRequestReserveDTO dto){
         return reserveService.updateReserve(dto);
     }
 
-    @PostMapping("/api/reserve/delete") // 검증완료
+    @DeleteMapping("/api/reserve") // 검증완료
     private Reserve delete(@RequestBody Long reserveId){
         return reserveService.deleteReserve(reserveId);
     }
