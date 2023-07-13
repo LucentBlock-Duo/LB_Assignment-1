a0053b326ce0d5b2bf26073df706e1b7549cda81
OneEpitome <rkddlfah0319@gmail.com>
2023-07-13 14:41:05 +0900

    feat : ItemDetail, RepairMan, MaintenanceItem 레포지토리 추가
    Feature
    
    ItemDetail, RepairMan, MaintenanceItem 레포지토리를 추가하였습니다.

---
+++ assignment-2/src/main/java/com/lucentblock/assignment2/repository/RepairManRepository.java
@@ -0,0 +1,7 @@
+package com.lucentblock.assignment2.repository;
+
+import com.lucentblock.assignment2.entity.RepairMan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepairManRepository extends JpaRepository<RepairMan, Long> {
+}
